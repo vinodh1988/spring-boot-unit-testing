19d1cac07576545543d808a856e0ebec3549c7ac
vinodh1988 <k.c.vinodh@hotmail.com>
2021-09-14 11:21:24 +0530

    unit-test-cases

--- src/test/java/com/rest/RestFirstAppApplicationTests.java
+++ src/test/java/com/rest/RestFirstAppApplicationTests.java
@@ -1,17 +1,45 @@
 package com.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rest.controllers.FirstController;
+import com.rest.services.IDataService;
 
+@SpringBootTest
 class RestFirstAppApplicationTests {
 
+	@Autowired
+	  IDataService idata;
 	
+	  IDataService idata2;
+	
+	  
 	  @Test
+	  @DisplayName("Controller greet method should work")
 	  public void firstTest() {
 		  FirstController fc=new FirstController();
 		  assertEquals(fc.greet(),"First Spring boot application" );
+	  }
+	  
+	  
+	  @Test
+	  @DisplayName("Autowiring of service must work")
+	  public void serviceTest() {
+		  
+		  assertTrue(idata!=null);
+	  }
+	  
+	  
+	  @Test
+	  @DisplayName("Not Autowired must be null")
+	  public void serviceTest2() {
+		  
+		  assertTrue(idata2==null);
 	  }
 }
