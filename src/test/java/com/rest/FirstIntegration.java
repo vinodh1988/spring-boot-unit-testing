608aea1da59b85aac43b86e71e548e2a09412774
vinodh1988 <k.c.vinodh@hotmail.com>
2021-09-15 14:39:59 +0530

    mockito-second-flow

--- src/test/java/com/rest/FirstIntegration.java
+++ src/test/java/com/rest/FirstIntegration.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.rest.controllers.FirstController;
 import com.rest.model.Person;
 import com.rest.repository.PeopleRepository;
 import com.rest.services.DataService;
@@ -21,20 +23,45 @@ import com.rest.services.DataService;
 public class FirstIntegration {
 	@Mock 
 	PeopleRepository peopleRepo;
+	@Mock
+	DataService service2;
+	@InjectMocks
+	FirstController controller;
 	@InjectMocks
 	DataService service;
-	@Test
-	@DisplayName("Testing Service Call Flow")
-    public void TestServiceCall() {
-		List<Person> list = new ArrayList<Person>();
+	
+	
+static	List<Person> list;
+	
+	@BeforeAll
+	public static void setup() {
+		list = new ArrayList<Person>();
 		list.add(new Person(1,"Raj","Chennai"));
 		list.add(new Person(2,"Harry","Chennai"));
+	}
+	
+	@Test
+	@DisplayName("Testing Service Call Flow")
+    public void testServiceCall() {
+		
 		
 		when(peopleRepo.findAll()).thenReturn(list);
+		
 		List<Person> resultList=service.getPeople();
 		assertTrue(resultList.size()==2);
 		assertTrue(resultList.get(1).getName().contentEquals("Harry"));
 		
+	}
+	
+	@Test
+	@DisplayName("Testing Controller Flow")
+	public void testControllerCall() {
+		
+		when(service2.getPeople()).thenReturn(list);
+		List<Person> resultList=controller.getPeople();
+	    System.out.println(resultList.size());
+		assertTrue(resultList.size()==2);
+		assertTrue(resultList.get(1).getName().contentEquals("Harry"));
 	}
 
 }
